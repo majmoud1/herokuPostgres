6e9675524e46d532e4e1abc7837b052d9ef1a905
majmoud1 <53261358+majmoud1@users.noreply.github.com>
2019-07-24 12:49:04 +0000

    Update scrypt.py

--- scrypt.py
+++ scrypt.py
@@ -1,17 +1,13 @@
 from flask import Flask, url_for, render_template, redirect
 import psycopg2 as psy
+import os
 app = Flask(__name__)
 
 #Connexion à la base de données 
 def connexionDB():
-    serveur = "ec2-174-129-227-128.compute-1.amazonaws.com"
-    bd = "def6p9u6t0o45m"
-    userBD = "zdfyfzhzxntvpi"
-    mdp = "e691227410d4268e9699a16c8d2b80b902994a58c189d01f7226f6ef75b478e7"
-    port ="5432"
-    #uri = "postgres://zdfyfzhzxntvpi:e691227410d4268e9699a16c8d2b80b902994a58c189d01f7226f6ef75b478e7@ec2-174-129-227-128.compute-1.amazonaws.com:5432/def6p9u6t0o45m"
     try:
-        connexion = psy.connect(host=serveur, database=bd, user=userBD, password=mdp, port=port)
+        DATABASE_URL = os.environ['DATABASE_URL']
+        connexion = psy.connect(DATABASE_URL, sslmode='require')
         print('ok')
         return connexion
     except psy.OperationalError:
